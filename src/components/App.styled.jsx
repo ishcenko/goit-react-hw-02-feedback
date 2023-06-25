a22ec42ca1cf68ac08e24a92cbc37c7ca6b02e0f
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-06-25 15:02:48 +0300

    App styled

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -14,7 +14,18 @@ export const Container = styled.div`
     display: block;
     padding: 25px;
     padding-bottom: 100px;
-    background: repeating-linear-gradient(22deg, #f6dada33, #cecef3 10%, #b8f1b8 33%, #f3d38c );
+    /* background: repeating-linear-gradient(36deg, #6c53fa33 , #eaff32 12% , #56ef56 15%, #f5a906 10% ); */
+    background-image: linear-gradient(
+  36deg,
+  rgb(246, 198, 195, 0.5) 10%,
+  #b0f8c6 30%,
+  #ade4f8 45%,
+  #f8dfa0
+);
+
+/* background: #8b82e1;  fallback for old browsers */
+/* background: -webkit-linear-gradient(36deg, #fdeff9, #ec38bc, #7303c0, #877cea);  Chrome 10-25, Safari 5.1-6 */
+/* background: linear-gradient(36deg, #f3dfee, #e58dcd, #c07eec, #948af4); W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 `
 
