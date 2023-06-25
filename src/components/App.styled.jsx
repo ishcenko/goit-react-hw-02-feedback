95cd2fa9e819d9fd75d920ee883d146dccddf341
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-06-25 14:40:42 +0300

    styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+    margin-top: 50px;
+    font-size: 24px;
+    color: rgb(2, 58, 114, 0.95);
+    margin: 50;
+    margin-right: auto; 
+    margin-left: auto;
+    border: 1px solid black;
+    border-radius: 25px;
+    width: 500px;
+    height: 500px;
+    display: block;
+    padding: 25px;
+    padding-bottom: 100px;
+    background: repeating-linear-gradient(22deg, #f6dada33, #cecef3 10%, #b8f1b8 33%, #f3d38c );
+
+`
+
